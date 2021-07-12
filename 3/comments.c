c2e278bc5b797e0b33886dd3df8702b5647738a1
Ayaan Retiwala <ayaan.180410107098@gmail.com>
2021-07-12 15:32:18 +0530

    some renaming and 3rd practical done

---
+++ 3/comments.c
@@ -0,0 +1,10 @@
+// Single-line comments start with // - only available in C99 and later.
+#include <stdio.h>
+
+/*
+Multi-line comments look like this. They work in C89 as well.
+*/
+
+// Constants: #define <keyword>
+// Constants are written in all-caps out of convention, not requirement
+#define DAYS_IN_YEAR 365
